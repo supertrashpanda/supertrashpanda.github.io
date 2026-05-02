56876d50b2ce40f31e417936e0742e4b12646051
supertrashpanda <superlingyunfan@gmail.com>
2026-05-02 03:00:54 +0000

    Deploying to gh-pages from @ supertrashpanda/supertrashpanda.github.io@aef460bbccc3d30abb2e288d37fbf209e31716a5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -575,7 +575,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%79%6B%6C%35%38%34%37@%70%73%75.%65%64%75", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -597,13 +597,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
