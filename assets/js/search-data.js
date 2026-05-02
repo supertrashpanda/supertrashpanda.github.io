385f0e21da8a093d5b757e5a3ae21e258b5a742c
supertrashpanda <superlingyunfan@gmail.com>
2026-05-02 04:39:33 +0000

    Deploying to gh-pages from @ supertrashpanda/supertrashpanda.github.io@43aacbf142ca8a86b69827b52e08fcaff06a86d1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -578,13 +578,6 @@ ninja.data = [{
           window.open("mailto:%79%6B%6C%35%38%34%37@%70%73%75.%65%64%75", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://medium.com/@117020170", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
